16ea3a58f3e531237feea80e94af5f39d4d76e6b
liarboss <124275630@qq.com>
2022-04-12 15:43:58 +0800

    update

--- src/3_机器学习/06_数据预处理/01_预处理数据.py
+++ src/3_机器学习/06_数据预处理/01_预处理数据.py
@@ -127,4 +127,86 @@ max_abs_scaler.scale_
 它们对你的数据的中心和范围使用更有鲁棒性的估计。
 """
 
+"""
+5.3.2 非线性转换
+有两种类型的转换是可用的:分位数转换和幂函数转换。分位数和幂变换都基于特征的单调变换，从而保持了每个特征值的秩。
+"""
 
+# 5.3.2.1 映射到均匀分布
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X, y = iris.data, iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+quantile_transformer = preprocessing.QuantileTransformer(random_state=0)
+X_train_trans = quantile_transformer.fit_transform(X_train)
+X_test_trans = quantile_transformer.transform(X_test)
+print(np.percentile(X_train[:, 0], [0, 25, 50, 75, 100]))
+# array([ 4.3,  5.1,  5.8,  6.5,  7.9])
+
+print(np.percentile(X_train_trans[:, 0], [0, 25, 50, 75, 100]))
+# array([ 0.00... ,  0.24...,  0.49...,  0.73...,  0.99... ])
+
+np.percentile(X_test[:, 0], [0, 25, 50, 75, 100])
+# array([ 4.4  ,  5.125,  5.75 ,  6.175,  7.3  ])
+np.percentile(X_test_trans[:, 0], [0, 25, 50, 75, 100])
+# array([ 0.01...,  0.25...,  0.46...,  0.60... ,  0.94...])
+
+
+# 5.3.2.2 映射到高斯分布
+# 在许多建模场景中，需要数据集中的特征的正态化。幂变换是一类参数化的单调变换， 其目的是将数据从任何分布映射到尽可能接近高斯分布，以便稳定方差和最小化偏斜。
+
+pt = preprocessing.PowerTransformer(method='box-cox', standardize=False)
+X_lognormal = np.random.RandomState(616).lognormal(size=(3, 3))
+X_lognormal
+# array([[1.28..., 1.18..., 0.84...],
+#        [0.94..., 1.60..., 0.38...],
+#        [1.35..., 0.21..., 1.09...]])
+pt.fit_transform(X_lognormal)
+# array([[ 0.49...,  0.17..., -0.15...],
+#        [-0.05...,  0.58..., -0.57...],
+#        [ 0.69..., -0.84...,  0.10...]])
+
+
+## 我们也可以 使用类 QuantileTransformer (通过设置 output_distribution='normal')把数据变换成一个正态分布。下面是将其应用到iris dataset上的结果:
+quantile_transformer = preprocessing.QuantileTransformer(output_distribution='normal', random_state=0)
+X_trans = quantile_transformer.fit_transform(X)
+quantile_transformer.quantiles_
+# array([[4.3, 2. , 1. , 0.1],
+#        [4.4, 2.2, 1.1, 0.1],
+#        [4.4, 2.2, 1.2, 0.1],
+#        ...,
+#        [7.7, 4.1, 6.7, 2.5],
+#        [7.7, 4.2, 6.7, 2.5],
+#        [7.9, 4.4, 6.9, 2.5]])
+
+
+"""
+5.3.3 归一化
+归一化 是 缩放单个样本以具有单位范数 的过程。如果你计划使用二次形式(如点积或任何其他核函数)来量化任何样本间的相似度，则此过程将非常有用。
+"""
+# 函数 normalize 提供了一个快速简单的方法在类似数组的数据集上执行操作，使用 l1 或 l2 范式:
+
+X = [[1., -1., 2.],
+     [2., 0., 0.],
+     [0., 1., -1.]]
+X_normalized = preprocessing.normalize(X, norm='l2')
+
+print(X_normalized)
+# array([[ 0.40..., -0.40...,  0.81...],
+#  [ 1.  ...,  0.  ...,  0.  ...],
+#  [ 0.  ...,  0.70..., -0.70...]])
+
+
+normalizer = preprocessing.Normalizer().fit(X)  # fit does nothing
+normalizer
+# Normalizer(copy=True, norm='l2')
+# 在这之后归一化实例可以被使用在样本向量中，像任何其他转换器一样:
+normalizer.transform(X)
+# array([[ 0.40..., -0.40...,  0.81...],
+#  [ 1.  ...,  0.  ...,  0.  ...],
+#  [ 0.  ...,  0.70..., -0.70...]])
+
+normalizer.transform([[-1., 1., 0.]])
+# array([[-0.70...,  0.70...,  0.  ...]])
