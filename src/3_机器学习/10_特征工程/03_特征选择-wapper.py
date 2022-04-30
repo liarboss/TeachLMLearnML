35abda5834c4af831bcd560fca3f45a6a9106ac2
liarboss <124275630@qq.com>
2022-04-30 21:42:51 +0800

    update

--- src/3_机器学习/10_特征工程/03_特征选择-wapper.py
+++ src/3_机器学习/10_特征工程/03_特征选择-wapper.py
@@ -30,20 +30,20 @@ print(X_new[:1])  # [[3.5 0.2]]
 # 使用带惩罚项的基模型，除了筛选出特征外，同时也进行了降维。使用feature_selection库的SelectFromModel类结合带L1惩罚项的逻辑回归模型，
 # 来选择特征的代码如下：
 
-import  numpy as np
-from sklearn.feature_selection import SelectFromModel
-from sklearn.linear_model import LogisticRegression
-from sklearn import datasets
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-# 带L1惩罚项的逻辑回归作为基模型的特征选择
-model = SelectFromModel(LogisticRegression(penalty="l1", C=0.1), max_features=2)
-model.fit_transform(iris.data, iris.target) # 传入数据
-# 返回模型选择的变量的数据内容
-X_new = model.fit_transform(X, Y)
-print(X[:1])# [[5.1 3.5 1.4 0.2]]
-print(X_new[:1])# [[3.5 1.4]]
+# import  numpy as np
+# from sklearn.feature_selection import SelectFromModel
+# from sklearn.linear_model import LogisticRegression
+# from sklearn import datasets
+# iris = datasets.load_iris()
+# X = iris.data
+# Y = iris.target
+# # 带L1惩罚项的逻辑回归作为基模型的特征选择
+# model = SelectFromModel(LogisticRegression(penalty="l1", C=0.1), max_features=2)
+# model.fit_transform(iris.data, iris.target) # 传入数据
+# # 返回模型选择的变量的数据内容
+# X_new = model.fit_transform(X, Y)
+# print(X[:1])# [[5.1 3.5 1.4 0.2]]
+# print(X_new[:1])# [[3.5 1.4]]
 
 
 # 基于树的特征选择
@@ -61,15 +61,15 @@ model = SelectFromModel(clf, prefit=True)
 X_new = model.transform(X)
 print(X_new.shape)
 
-import numpy as np
-from sklearn.feature_selection import SelectFromModel
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn import datasets
-
-iris = datasets.load_iris()
-
-# 带L1惩罚项的逻辑回归作为基模型的特征选择
-model = SelectFromModel(GradientBoostingClassifier())
-model.fit_transform(iris.data, iris.target)  # 传入数据
-print(model.fit_transform(iris.data, iris.target))  # 返回模型选择的变量的数据内容
+# import numpy as np
+# from sklearn.feature_selection import SelectFromModel
+# from sklearn.ensemble import GradientBoostingClassifier
+# from sklearn import datasets
+#
+# iris = datasets.load_iris()
+#
+# # 带L1惩罚项的逻辑回归作为基模型的特征选择
+# model = SelectFromModel(GradientBoostingClassifier())
+# model.fit_transform(iris.data, iris.target)  # 传入数据
+# print(model.fit_transform(iris.data, iris.target))  # 返回模型选择的变量的数据内容
 
